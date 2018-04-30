7c35c566295c452b6e2218f1a154b37390950052
Ariana B <alb886@nyu.edu>
2018-04-30 17:21:01 -0400

    Some Enemy/Enemy Fire Code, and more folders

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour {
+
+
+	[SerializeField]
+	GameObject fireball;
+
+	//for shooting fireballs
+	float fireRate;
+	float nextFire;
+
+	//forStunning
+	public bool isStunned; 
+
+
+	// Use this for initialization
+	void Start () {
+
+		fireRate = 4f;
+		nextFire = Time.time;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//if isStunned is true, enemy stops moving/attacking and can be picked up
+		if (isStunned == true) {
+
+
+
+		}
+		
+	}
+	//If player bullet hits enemy, enemy is destroyed
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		if (col.gameObject.tag == "Fireball")
+		{
+			//Destroy (col.gameObject);
+			//Destroy (gameObject);
+			isStunned = true;
+			Debug.Log ("Player has hit Enemy! Enemy stunned.");
+		}
+	}
+	void CheckIfTimeToFire ()
+	{
+		if (Time.time > nextFire)
+		{
+			Instantiate (fireball, transform.position, Quaternion.identity);
+			nextFire = Time.time + fireRate;
+			}
+		else
+		{
+
+		}
+	}
+
+}
---
+++ Assets/Scripts/EnemyFireball.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFireball : MonoBehaviour {
+
+	float moveSpeed = 3f;
+
+	private Rigidbody2D rb;
+
+	playerBehavior target;
+	Vector2 moveDirection;
+
+	// Use this for initialization
+	void Start () {
+
+		rb = GetComponent<Rigidbody2D> ();
+		//so bullets find player
+		target = GameObject.FindObjectOfType<playerBehavior> ();
+		//so bullets move
+		moveDirection = (target.transform.position - transform.position).normalized * moveSpeed;
+		//bullet speed
+		rb.velocity = new Vector2 (moveDirection.x, moveDirection.y);
+		//bullet is destroyed after time
+		Destroy (gameObject, 3f);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D (Collider2D col){
+		//if enemy bullet hits player, it is destroyed
+		if (col.gameObject.name == "Player") {
+			Debug.Log ("Enemy fireball has Hit!");
+			Destroy (gameObject);
+		}
+		//if enemy bullet hits object, it is destroyed
+		if (col.gameObject.tag == "object") {
+			Debug.Log ("Hit surface!");
+			Destroy (gameObject);
+		}
+	}
+}
