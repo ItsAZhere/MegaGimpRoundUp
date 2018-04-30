f71cc2d9b2cf9d0b682af604410791a9b59dc032
Anna <annazhang.1997@gmail.com>
2018-04-30 17:01:35 -0400

    PlayerMovement Script

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -19,40 +19,53 @@ public class playerMovement : MonoBehaviour {
 	}
 
 
+    void FixedUpdate()
+
+    {
+
+        // "snap" player position to new position
+        if (moveToPoint)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, endPosition, Speed * Time.deltaTime);
+        }
+
+    }
+
+
+
     void Update(){
 
+
         // going left
-        if (Input.GetKey(KeyCode.A)){
+        if (Input.GetKeyDown(KeyCode.A)){
             endPosition = new Vector3(endPosition.x - distanceToGo, endPosition.y, endPosition.z);
+            moveToPoint = true;
         }
 
         // going right
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.D))
         {
             endPosition = new Vector3(endPosition.x + distanceToGo, endPosition.y, endPosition.z);
+            moveToPoint = true;
         }
 
         // going forward
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W))
         {
             endPosition = new Vector3(endPosition.x, endPosition.y + distanceToGo, endPosition.z);
+            moveToPoint = true;
         }
 
         //going backward
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKeyDown(KeyCode.S))
         {
             endPosition = new Vector3(endPosition.x, endPosition.y - distanceToGo, endPosition.z);
+            moveToPoint = true;
         }
 
     }
 	
 	
-	void FixedUpdate () {
-
-        // "snap" player position to new position
-        if (moveToPoint == true){
-            transform.position = Vector3.MoveTowards(transform.position, endPosition, Speed * Time.deltaTime);
-        }
-	}
+	
 
 }
