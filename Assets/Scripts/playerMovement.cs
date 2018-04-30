5d3a4b3de63b760dba49aaa30fdc41fa887f5a8b
Anna <annazhang.1997@gmail.com>
2018-04-30 16:08:59 -0400

    Added Player Movement script

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+    [SerializeField] private float distanceToGo;
+    [SerializeField] private float Speed;
+
+    private bool moveToPoint = false;
+    private Vector3 endPosition;
+
+
+	void Start () {
+
+        // current position at beginning 
+        endPosition = transform.position;
+		
+	}
+
+
+    void Update(){
+
+        // going left
+        if (Input.GetKey(KeyCode.A)){
+            endPosition = new Vector3(endPosition.x - distanceToGo, endPosition.y, endPosition.z);
+        }
+
+        // going right
+        if (Input.GetKey(KeyCode.D))
+        {
+            endPosition = new Vector3(endPosition.x + distanceToGo, endPosition.y, endPosition.z);
+        }
+
+        // going forward
+        if (Input.GetKey(KeyCode.W))
+        {
+            endPosition = new Vector3(endPosition.x, endPosition.y + distanceToGo, endPosition.z);
+        }
+
+        //going backward
+        if (Input.GetKey(KeyCode.S))
+        {
+            endPosition = new Vector3(endPosition.x, endPosition.y - distanceToGo, endPosition.z);
+        }
+
+    }
+	
+	
+	void FixedUpdate () {
+
+        // "snap" player position to new position
+        if (moveToPoint == true){
+            transform.position = Vector3.MoveTowards(transform.position, endPosition, Speed * Time.deltaTime);
+        }
+	}
+
+}
