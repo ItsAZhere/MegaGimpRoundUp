45ae2c174a178efd2f33c7221a602f9197290a20
Unknown <hsc362@nyu.edu>
2018-05-02 16:02:16 -0400

    All Buttons Inserted

--- Assets/Scripts/buttons.cs
+++ Assets/Scripts/buttons.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class buttons : MonoBehaviour {
 
@@ -12,5 +13,15 @@ public class buttons : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public void StartButtonPressed(){
+		SceneManager.LoadScene("Level 1");
+	}
+	public void InstructionsButtonPressed(){
+		SceneManager.LoadScene ("Instruction");
+	}
+	public void MenuButtonPressed(){
+		SceneManager.LoadScene("Start");
 	}
 }
